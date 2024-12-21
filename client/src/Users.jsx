dc646d2381be9834050df5b510af5055de487d7b
sneh0102 <ladsneh05@gmail.com>
2024-12-21 15:08:42 +0530

    first commit

---
+++ client/src/Users.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Users() {
+  return (
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+      <div className="w-50 bg-white rounded p-3">
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Age</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+        </table>
+      </div>
+    </div>
+  )
+}
